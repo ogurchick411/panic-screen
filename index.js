90aff50e4d73f1aaf3bd3633c7b3a5ac2b2b264d
Ogurchick <148265956+ogurchick411@users.noreply.github.com>
2026-07-24 15:02:58 +0200

    hide app icon from dock for stealth mode

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const { app, BrowserWindow } = require('electron');
 app.whenReady().then(() => {
   console.log('App ready');
 });
+
+const { app } = require('electron');
+
+app.whenReady().then(() => {
+  if (process.platform === 'darwin') app.dock.hide();
+});
